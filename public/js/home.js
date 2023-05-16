9dc5c0f62a0f1fb5f0c0fd71919c62f53515a6a3
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-05-16 10:59:39 +0200

    trending ok

--- public/js/home.js
+++ public/js/home.js
@@ -17,17 +17,17 @@ async function getData(url) {
 getData('https://api.themoviedb.org/3/trending/movie/day?language=fr-FR&page=1').then(items => {
     trendingMoviesDiv = (document.createElement('div'));
     trendingMoviesDiv.classList.add('popular_div');
+    trendingMoviesDiv.innerHTML = '<h2>Films tendance';
     for (let i = 0; i < 4; i++) {
         console.log(items[i])
         trendingMoviesDiv.innerHTML += (`
-            <div class="polular_div">
-                <div class="item_div">
-                    <div class="image_container">
-                        <img src="https://image.tmdb.org/t/p/w342/${items[i].poster_path}">
-                    </div>
-                    <h3>${items[i].title}</h3>
+            <div class="item_div">
+                <div class="image_container">
+                    <img src="https://image.tmdb.org/t/p/w342/${items[i].poster_path}">
                 </div>
-            </div>`)
+                <h3>${items[i].title}</h3>
+            </div>
+        `)
     }
     mainContainer.appendChild(trendingMoviesDiv)
 })
@@ -35,17 +35,17 @@ getData('https://api.themoviedb.org/3/trending/movie/day?language=fr-FR&page=1')
 getData('https://api.themoviedb.org/3/trending/tv/day?language=fr-FR&page=1').then(items => {
     trendingTvDiv = (document.createElement('div'));
     trendingTvDiv.classList.add('popular_div');
+    trendingTvDiv.innerHTML = '<h2>Series tendance';
     for (let i = 0; i < 4; i++) {
         console.log(items[i])
         trendingTvDiv.innerHTML += (`
-            <div class="polular_div">
                 <div class="item_div">
                     <div class="image_container">
                         <img src="https://image.tmdb.org/t/p/w342/${items[i].poster_path}">
                     </div>
                     <h3>${items[i].name}</h3>
                 </div>
-            </div>`)
+            `)
     }
     mainContainer.appendChild(trendingTvDiv)
 })
